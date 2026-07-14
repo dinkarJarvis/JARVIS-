60db67cb485b495f0899ec5e41db041ceee40e63
dinkarJarvis <dinkarr31@gmail.com>
2026-07-14 10:35:05 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+// ======================================
+// JARVIS Service Worker
+// ======================================
+
+self.addEventListener("install", event => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+    // Network-first for now.
+});
